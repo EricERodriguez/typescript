848f73b7a634473c1385f607914d1be5f009d761
EricERodriguez <eric92rodriguez@gmail.com>
2021-07-12 13:25:53 -0300

    desestructuration

--- app.js
+++ app.js
@@ -11,5 +11,17 @@
         console.log(clave);
         console.log(poder);
     };
-    extraer(avenger);
+    // extraer (avenger);
+    var avengers = ["Thor", "Ironman", "Spiderman"];
+    var loki = avengers[0], hombre = avengers[1], arana = avengers[2];
+    // console.log ( loki );
+    // console.log ( hombre );
+    // console.log ( arana );
+    var extraerArr = function (_a) {
+        var thor = _a[0], ironman = _a[1], spiderman = _a[2];
+        console.log(thor);
+        console.log(ironman);
+        console.log(spiderman);
+    };
+    extraerArr(avengers);
 })();
