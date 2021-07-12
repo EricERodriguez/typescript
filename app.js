1ceee48abbff3ed8da422789b73a84d4627ce96c
EricERodriguez <eric92rodriguez@gmail.com>
2021-07-12 13:46:05 -0300

    promise

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 "use strict";
-(function () {
-    var avenger = {
-        nombre: 'Steve',
-        clave: 'Capitan america',
-        poder: 'Drogas'
-    };
-    var extraer = function (avenger) {
-        var nombre = avenger.nombre, clave = avenger.clave, poder = avenger.poder;
-        console.log(nombre);
-        console.log(clave);
-        console.log(poder);
-    };
-    // extraer (avenger);
-    var avengers = ["Thor", "Ironman", "Spiderman"];
-    var loki = avengers[0], hombre = avengers[1], arana = avengers[2];
-    // console.log ( loki );
-    // console.log ( hombre );
-    // console.log ( arana );
-    var extraerArr = function (_a) {
-        var thor = _a[0], ironman = _a[1], spiderman = _a[2];
-        console.log(thor);
-        console.log(ironman);
-        console.log(spiderman);
-    };
-    extraerArr(avengers);
+(() => {
+    console.log(`inicio`);
+    const prom1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(`Se termino el timeout`);
+        }, 1000);
+    });
+    prom1
+        //resolucion
+        .then(mensaje => console.log(mensaje))
+        //si hay error
+        .catch(err => console.warn(err));
+    console.log(`Fin`);
 })();
