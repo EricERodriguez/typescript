2e26ada890e55c2607e9389ff3d8cfbca1615818
EricERodriguez <eric92rodriguez@gmail.com>
2021-07-12 14:12:27 -0300

    promise2

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
 "use strict";
 (() => {
-    console.log(`inicio`);
-    const prom1 = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            reject(`Se termino el timeout`);
-        }, 1000);
-    });
-    prom1
-        //resolucion
-        .then(mensaje => console.log(mensaje))
-        //si hay error
-        .catch(err => console.warn(err));
-    console.log(`Fin`);
 })();
--- app.ts
+++ app.ts
@@ -1,19 +1,4 @@
 ( () => {
 
-    console.log(`inicio`);
-
-    const prom1 = new Promise( (resolve, reject) => {
     
-    setTimeout (() => {
-        reject(`Se termino el timeout`)
-    }, 1000)
-    });
-    
-    prom1
-        //resolucion
-        .then( mensaje => console.log (mensaje))
-        //si hay error
-        .catch( err => console.warn ( err ))
-
-    console.log(`Fin`);
 })();
