5609faceb54ce4afcd21b1d43480b443554ee72b
EricERodriguez <eric92rodriguez@gmail.com>
2021-07-13 00:55:18 -0300

    disc app

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
 "use strict";
 (() => {
-    const enviarMision = (xmen) => {
-        console.log(`Enviando a ${xmen.nombre} a la mision`);
-    };
-    const regresarAlCuartel = (xmen) => {
-        console.log(`regresando a ${xmen.nombre} a la mision`);
-    };
-    const wolverine = {
-        nombre: `logan`,
-        edad: 60
-    };
-    enviarMision(wolverine);
-    regresarAlCuartel(wolverine);
 })();
--- app.ts
+++ app.ts
@@ -1,24 +1,4 @@
 ( () => {
 
-    interface Xmen {
-        nombre: string;
-        edad: number;
-        poder?: string;
-    }
-
-    const enviarMision = (xmen: Xmen) => {
-        console.log(`Enviando a ${xmen.nombre} a la mision`)
-    }
-
-    const regresarAlCuartel = (xmen: Xmen) => {
-        console.log(`regresando a ${xmen.nombre} a la mision`)
-    }
-
-    const wolverine: Xmen = {
-        nombre: `logan`,
-        edad: 60
-    }
-
-    enviarMision ( wolverine);
-    regresarAlCuartel ( wolverine);
+    
 })();
